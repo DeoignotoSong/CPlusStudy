538c18885463c0c2fac9e7ad7dd1156ced163eb7
Deoignoto <deoignoto@outlook.com>
2019-09-24 07:23:09 +0800

    Create project

---
+++ CPlusStudy/CPlusStudy.cpp
@@ -0,0 +1,44 @@
+﻿// CPlusStudy.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include "ThostFtdcMdApi.h"
+#include "CMdHandler.h"
+#include "CTraderHandler.h"
+#include "ThostFtdcTraderApi.h"
+#include <Windows.h>
+#include <direct.h>
+#include "getconfig.h"
+#include "FileReader.h"
+
+using namespace std;
+
+int main()
+{
+	// 保存生成的log文件的文件夹路径
+	string logFilePath = getConfig("config", "LogFilesPath");
+
+	// 创建保存生成的log文件的文件夹
+	// folderCreatedResult：
+	// 0：创建成功
+	// -1：文件夹存在，创建失败
+	int folderCreatedResult = _mkdir(logFilePath.c_str());
+
+	if (folderCreatedResult == 0) {
+		cout << "Create log directory success." << endl;
+	}
+	else {
+		cout << "Log directory is already existed." << endl;
+	}
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
---
+++ CPlusStudy/getConfig.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+#include <wtypes.h>
+#include <conio.h>
+#include <iostream>
+#include <locale>
+#include <string>
+#include <fstream>
+#include <conio.h>
+#include <time.h>
+#include <sstream>
+#include <locale.h>
+#include <vector>
+
+using namespace std;
+
+string getConfig(string title, string cfgName);
